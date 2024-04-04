0db1c8d9acfdb775b4b4ca2c702c01648a1065b0
huangjinchao <grozta@163.com>
2024-04-04 13:01:13 +0800

    修改了全监督的训练方案脚本

--- code/supervised_run.sh
+++ code/supervised_run.sh
@@ -1,4 +1,4 @@
-
-CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D.py --root_path /home/grozta/Desktop/DATASET/ACDC --exp ACDC/unet --model unet  --max_iterations 10000 --batch_size 18 --seed 5179 && \
-CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D_ViT.py --root_path /home/grozta/Desktop/DATASET/ACDC --exp ACDC/swinunet --model swinunet --max_iterations 10000 --batch_size 18 --seed 5179 && \
-CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D_VIM.py --root_path /home/grozta/Desktop/DATASET/ACDC --exp ACDC/VIM --model mambaunet --max_iterations 10000 --batch_size 18
+CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D.py --root_path /home/grozta/Desktop/DATASET/ACDC --labeled_num 140  --exp ACDC/unet --model unet  --max_iterations 10000 --batch_size 18 --seed 5179 && \
+CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D.py --root_path /home/grozta/Desktop/DATASET/ACDC --labeled_num 140 --exp ACDC/vnet --model vnet --vnet_n_filters 64 --max_iterations 10000 --batch_size 18 --seed 5179 && \
+CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D_ViT.py --root_path /home/grozta/Desktop/DATASET/ACDC --labeled_num 140  --exp ACDC/swinunet --model swinunet --max_iterations 20000 --batch_size 18 --seed 5179 && \
+CUDA_VISIBLE_DEVICES=0 python train_fully_supervised_2D_VIM.py --root_path /home/grozta/Desktop/DATASET/ACDC --labeled_num 140 --exp ACDC/VIM --model mambaunet --max_iterations 14000 --batch_size 18
